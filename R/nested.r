c3008f1da53529b921d2ced8227d242a8967b9b9
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2016-02-10 15:01:57 +0100

    find nested blocks and dot blocks

---
+++ R/nested.r
@@ -0,0 +1,70 @@
+examples.nested = function() {
+  setwd("D:/libraries/rmdtools")
+
+  dot.levels = c(
+    exercise = 0,
+    frame = 1,
+    column = 2,
+    success = 3,
+    when = 3
+  )
+
+  txt = readLines("test.rmd")
+  df = find.rmd.nested(txt, dot.levels)
+
+  parent.types = c("frame","column","chunk")
+  pt = get.levels.parents.by.types(df$level, df$type, parent.types)
+
+  d = cbind(df,pt)
+}
+
+find.rmd.nested = function(txt, dot.levels = NULL) {
+  restore.point("find.rmd.nested")
+
+  bdf = find.rmd.blocks(txt)
+  cdf = find.rmd.chunks(txt) %>% select(start,end) %>% mutate(type="chunk", arg.str = NA_character_)
+  ddf = find.dot.blocks(txt, dot.levels)
+
+  df = rbind(ddf,bdf,cdf) %>% arrange(start,-end)
+  df$level = get.start.end.levels(df$start, df$end)
+  df$parent = get.levels.parents(df$level)
+  df
+}
+
+#' Find blocks that have only a starting line of the form
+#'
+#' #. type arguments
+#'
+find.dot.blocks = function(txt,dot.levels = NULL, dot.start = "#. ") {
+  restore.point("find.dot.blocks")
+
+  rows = str.starts.with(txt, dot.start)
+  str = txt[rows]
+  n = length(str)
+  if (n == 0) return(NULL)
+
+  type = str.right.of(str,dot.start) %>% str.trim %>% str.left.of(" ")
+  arg.str = str.right.of(str,dot.start) %>% str.right.of(type) %>% str.trim
+  start = which(rows)
+
+  level = rep(Inf,n)
+  ml = type %in% names(dot.levels)
+
+  if (sum(ml)>0) {
+    level[ml] = dot.levels[type[ml]]
+    end =
+    elevel = c(level, -Inf)
+    end.ind = 2:(n+1)
+    while(TRUE) {
+      add.rows = which(elevel[end.ind] > level)
+      if (length(add.rows)==0) break
+      end.ind[add.rows] = end.ind[add.rows]+1
+    }
+    end = c(start-1,NROW(txt))[end.ind]
+  } else {
+    end = c(start[-1]-1,NROW(txt))
+  }
+
+  data.frame(start=start, end=end, type=type, arg.str = arg.str,stringsAsFactors = FALSE)
+
+}
--- R/tools.r
+++ R/tools.r
@@ -1,3 +1,99 @@
+#' Find the levels given ordered start and end positions of possible nested blocks
+#'
+#' @param start vector of start positions of the blocks
+#' @param end vector of end positions of the blocks
+#' @param start.level the initial level
+#' @return a vector of levels of each blocks, moore deeply nested blocks have higher levels
+get.start.end.levels = function(start, end, start.level = 1L) {
+  n = length(start)
+  if (n==0) return(NULL)
+  levels = rep(start.level,n)
+  end.stack = rep(NA_integer_,n)
+  end.stack.ind = 0
+
+  level = start.level
+  for (i in 1:n) {
+    if (end.stack.ind >0) {
+      while (start[i]>end.stack[end.stack.ind]) {
+        level = level-1L
+        end.stack.ind = end.stack.ind -1L
+        if (end.stack.ind == 0) break
+      }
+    }
+    levels[i] = level
+    level = level+1L
+    end.stack.ind = end.stack.ind+1L
+    end.stack[end.stack.ind] = end[i]
+  }
+  levels
+}
+
+#' Find the parent index of each row given a vector levels that describes the nestedness
+#'
+#' @param levels integer vector of levels must start with lowest level and increase by 1 or decrease to an integer number
+#' @return a vector of parent indices or 0 for most outer levels
+get.levels.parents = function(levels) {
+  n = length(levels)
+  if (n==0) return(NULL)
+  parents = rep(0,n)
+  min.level = levels[1]
+  if (min.level != 1)
+    levels = levels-min.level+1
+
+  level.parent = rep(NA_integer_,max(levels))
+
+  for (i in 1:n) {
+    if (levels[i]>1) {
+      parents[i] = level.parent[levels[i]-1]
+    }
+    level.parent[levels[i]] = i
+  }
+  parents
+}
+
+#' Find the parent index of each row given a vector levels that describes the nestedness
+#'
+#' @param levels integer vector of levels must start with lowest level and increase by 1 or
+#'        decrease to an integer number
+#' @param types character vector of types
+#' @param parent.types the parent.types that shall be characterized
+#' @return a matrix of parent type indices with length(levels) rows and length(parent.types) columns. If there is no parent type, we enter a 0.
+get.levels.parents.by.types = function(levels, types, parent.types = setdiff(unique(types),c(NA))) {
+  restore.point("get.levels.parents.by.types")
+
+  n = length(levels)
+  m = length(parent.types)
+  if (n==0) return(NULL)
+  min.level = levels[1]
+  if (min.level != 1)
+    levels = levels-min.level+1
+  tp = matrix(0,n,m)
+  max.level = max(levels)
+  ltp = matrix(0,max.level+1,m)
+  if (is.character(types)) {
+    colnames(ltp) = parent.types
+  }
+
+  i = 0
+  i = i+1
+  for (i in 1:n) {
+    tp[i,] = ltp[levels[i],]
+
+    # reset earlier levels
+    if (i>1) {
+      if (levels[i]<=levels[i-1]) {
+        ltp[(levels[i]:levels[i-1])+1,] = 0
+      }
+    }
+    if (types[i] %in% parent.types) {
+      ltp[(levels[i]:max.level)+1,types[i]] = i
+    }
+  }
+  colnames(tp) = paste0("parent_",parent.types)
+  tp
+}
+
+
 nlist = function (...)
 {
     li = list(...)
